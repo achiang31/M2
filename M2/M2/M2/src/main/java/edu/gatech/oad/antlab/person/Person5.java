25823657f57a0ffb53a41aa5ad39b3c5cbff7c5e
Xiying Huang <Xiying Huang>
2016-09-08 11:55:25 -0400

    Person 5 & AntLabMain Modified

--- M2/M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ M2/M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Xiying Huang
  *  @version 1.1
  */
 public class Person5 {
@@ -31,7 +31,13 @@ public class Person5 {
 	 */
 	private String calc(String input) {
 	  //Person 5 put your implementation here
-	  return null;
+		if (input.length() == 1) {
+			return input;
+		} else if (input.length() == 2) {
+			return input;
+		}
+		String temp = input.substring(2) + input.substring(0, 1);
+	  	return temp;
 	}
 	
 	/**
@@ -45,5 +51,4 @@ public class Person5 {
 	public String toString(String input) {
 	  return name + calc(input);
 	}
-
 }
